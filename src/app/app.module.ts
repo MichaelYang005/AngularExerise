153e689f6327e5f1ca64c2a2df2c5c64c24b15ee
Michael Yang <michaelyang005@gmail.com>
2018-10-20 16:21:33 -0400

    exerises2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import 'hammerjs';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 
 import { AppComponent } from './app.component';
@@ -21,6 +25,10 @@ import { MenuComponent } from './menu/menu.component';
     FlexLayoutModule,
     MatListModule,
     MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
+
    
   ],
   providers: [],
